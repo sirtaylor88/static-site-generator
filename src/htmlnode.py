21f1d5bd7fdd4dc3fce46a20e3017500c10e9aea
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2024-08-02 22:16:47 +0700

    ✨ Add leaf nodes.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,3 +32,34 @@ class HTMLNode:
     def __repr__(self) -> str:
         """String representantion of a HTMLNode instance."""
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    """HTML node."""
+
+    def __init__(
+        self, value: str, tag: tp.Optional[str] = None, props: tp.Optional[dict] = None
+    ) -> None:
+        """Inits LeafNode."""
+        super().__init__(tag, value, props=props)
+
+    def to_html(self) -> str:
+        """Overwrite `to_html` method.
+
+        Raises:
+            ValueError if leaf node has no value.
+
+        Returns:
+            A string.
+        """
+        if not self.value:
+            raise ValueError("Leaf node must have a value.")
+        if self.tag is None:
+            return self.value
+        if self.props_to_html():
+            return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        """String representantion of a LeafNode instance."""
+        return f"LeafNode({self.tag}, value: {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 @pytest.mark.parametrize(
@@ -43,3 +43,35 @@ def test_repr(n, expected):
     """Test __repr__ method."""
     node = HTMLNode(*n)
     assert str(node) == expected
+
+
+def test_init_leaf_node():
+    """Test __init__ method for LeafNode"""
+    with pytest.raises(TypeError) as excinfo:
+        LeafNode()
+    assert "missing 1 required positional argument: 'value'" in str(excinfo)
+
+    lnode = LeafNode("I am a Leaf Node")
+    assert str(lnode) == "HTMLNode(None, I am a Leaf Node, None, None)"
+    assert lnode.children is None
+
+
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        (["I am a Leaf Node"], "I am a Leaf Node"),
+        (["I am a Leaf Node", "p"], "<p>I am a Leaf Node</p>"),
+        (
+            ["I am a Leaf Node", "p", {"class": "text-center"}],
+            '<p class="text-center">I am a Leaf Node</p>',
+        ),
+    ],
+)
+def test_leaf_node_to_html(n, expected):
+    """Test `to_html` method from LeafNode."""
+    lnode = LeafNode(*n)
+    assert lnode.to_html() == expected
+
+    with pytest.raises(ValueError) as excinfo:
+        LeafNode("").to_html()
+    assert "Leaf node must have a value." in str(excinfo)
