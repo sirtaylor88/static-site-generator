c70f2f99d90c4e7945ca7ceb60c340ca760689f7
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2024-08-30 20:17:54 +0200

    ✨ Add split nodes with links or images.

--- src/test_utils.py
+++ src/test_utils.py
@@ -3,7 +3,13 @@
 import pytest
 
 from textnode import TextNode
-from utils import extract_markdown_images, extract_markdown_links, split_nodes_delimiter
+from utils import (
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
+)
 
 
 @pytest.mark.parametrize(
@@ -21,11 +27,28 @@ from utils import extract_markdown_images, extract_markdown_links, split_nodes_d
                 TextNode(", I'm here.", TextNode.TEXT),
             ],
         ),
+        (
+            [TextNode("Hello Tai, I'm **here.**", TextNode.TEXT)],
+            [
+                TextNode("Hello Tai, I'm ", TextNode.TEXT),
+                TextNode("here.", TextNode.BOLD),
+            ],
+        ),
     ],
 )
 def test_split_nodes_delimiter(data, expected):
     """Test that `split_nodes_delimiter` method works correctly."""
     assert split_nodes_delimiter(data, "**", TextNode.BOLD) == expected
+
+    with pytest.raises(TypeError) as excinfo:
+        split_nodes_delimiter(data, "**", TextNode.IMAGE)
+    assert "Invalid text type." in str(excinfo)
+
+    with pytest.raises(ValueError) as excinfo:
+        split_nodes_delimiter(
+            [TextNode("Hello Tai, **I'm here.", TextNode.TEXT)], "**", TextNode.BOLD
+        )
+    assert "Invalid markdown syntax." in str(excinfo)
 
 
 def test_extract_markdown_images():
@@ -49,4 +72,40 @@ def test_extract_markdown_links():
     assert extract_markdown_links(text) == [
         ("to boot dev", "https://www.boot.dev"),
         ("to youtube", "https://www.youtube.com/@bootdotdev"),
+    ]
+
+
+def test_split_nodes_image():
+    """Test that `split_nodes_image` method works correctly."""
+    node = TextNode(
+        (
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) "
+            "and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)."
+        ),
+        TextNode.TEXT,
+    )
+    assert split_nodes_image([node]) == [
+        TextNode("This is text with a ", TextNode.TEXT),
+        TextNode("rick roll", TextNode.IMAGE, "https://i.imgur.com/aKaOqIh.gif"),
+        TextNode(" and ", TextNode.TEXT),
+        TextNode("obi wan", TextNode.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+        TextNode(".", TextNode.TEXT),
+    ]
+
+
+def test_split_nodes_link():
+    """Test that `split_nodes_link` method works correctly."""
+    node = TextNode(
+        (
+            "This is text with a link [to boot dev](https://www.boot.dev) "
+            "and [to youtube](https://www.youtube.com/@bootdotdev)."
+        ),
+        TextNode.TEXT,
+    )
+    assert split_nodes_link([node]) == [
+        TextNode("This is text with a link ", TextNode.TEXT),
+        TextNode("to boot dev", TextNode.LINK, "https://www.boot.dev"),
+        TextNode(" and ", TextNode.TEXT),
+        TextNode("to youtube", TextNode.LINK, "https://www.youtube.com/@bootdotdev"),
+        TextNode(".", TextNode.TEXT),
     ]
--- src/utils.py
+++ src/utils.py
@@ -16,20 +16,36 @@ def split_nodes_delimiter(
         text_type: The type of the text.
 
     Returns:
-        A list of TextNode instances.
+        A new list of TextNode instances.
+
+    Raises:
+        TypeError: if text type is not valid.
+        ValueError: if there is a missing closing delimiter.
     """
     result = []
+    if text_type not in [TextNode.TEXT, TextNode.BOLD, TextNode.CODE, TextNode.ITALIC]:
+        raise TypeError("Invalid text type.")
+
     for node in old_nodes:
         if node.text_type != TextNode.TEXT:
             result.append(node)
             continue
-
-        if delimiter in node.text and node.text.count(delimiter) % 2 == 1:
+        if delimiter in node.text and node.text.count(delimiter) == 1:
             raise ValueError("Invalid markdown syntax.")
 
-        texts = node.text.split(delimiter)
-        for i, text in enumerate(texts):
-            result.append(TextNode(text, TextNode.TEXT if i % 2 == 0 else text_type))
+        substrings = node.text.split(delimiter, 2)
+        if len(substrings) < 3:
+            result.append(node)
+            continue
+        if substrings[0]:
+            result.append(TextNode(substrings[0], TextNode.TEXT))
+        result.append(TextNode(substrings[1], text_type))
+        if substrings[2]:
+            result.extend(
+                split_nodes_delimiter(
+                    [TextNode(substrings[2], TextNode.TEXT)], delimiter, text_type
+                )
+            )
 
     return result
 
@@ -60,3 +76,67 @@ def extract_markdown_links(text: str) -> list[tuple[str, str]]:
     pattern = r"(?<!!)\[(.*?)\]\((.*?)\)"
     re.findall(pattern, text)
     return re.findall(pattern, text)
+
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    """Split nodes having images.
+
+    Args:
+        old_nodes: A list of TextNode instances.
+
+    Returns:
+        A new list of TextNode instances.
+    """
+    result = []
+    for node in old_nodes:
+        if node.text_type != TextNode.TEXT:
+            result.append(node)
+            continue
+
+        image_data_list = extract_markdown_images(node.text)
+
+        if not image_data_list:
+            result.append(node)
+            continue
+
+        alt, src = image_data_list[0]
+        substrings = node.text.split(f"![{alt}]({src})", 1)
+        if substrings[0]:
+            result.append(TextNode(substrings[0], TextNode.TEXT))
+        result.append(TextNode(alt, TextNode.IMAGE, src))
+        if substrings[1]:
+            result.extend(split_nodes_image([TextNode(substrings[1], TextNode.TEXT)]))
+
+    return result
+
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    """Split nodes having links.
+
+    Args:
+        old_nodes: A list of TextNode instances.
+
+    Returns:
+        A new list of TextNode instances.
+    """
+    result = []
+    for node in old_nodes:
+        if node.text_type != TextNode.TEXT:
+            result.append(node)
+            continue
+
+        link_data_list = extract_markdown_links(node.text)
+
+        if not link_data_list:
+            result.append(node)
+            continue
+
+        text, href = link_data_list[0]
+        substrings = node.text.split(f"[{text}]({href})", 1)
+        if substrings[0]:
+            result.append(TextNode(substrings[0], TextNode.TEXT))
+        result.append(TextNode(text, TextNode.LINK, href))
+        if substrings[1]:
+            result.extend(split_nodes_link([TextNode(substrings[1], TextNode.TEXT)]))
+
+    return result
